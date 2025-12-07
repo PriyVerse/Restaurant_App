44b7aa51fb29049096f2ff4ae0637f4e18790b8d
PriyVerse <priyansunayak108@gmail.com>
2025-12-07 22:34:38 +0530

    Day 1-backend

---
+++ Backend/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+dotenv.config();
+
+const app = express();
+//database connection
+connectDB();
+//middlewares
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from server");
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
+});
